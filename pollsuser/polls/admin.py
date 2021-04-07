65d3c6630f81058c8cb0415d987ad4e7252ed92c
Shustrjak <shustrik4ever@rambler.ru>
2021-04-08 02:02:05 +0300

    .

--- pollsuser/polls/admin.py
+++ pollsuser/polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from polls.models import Poll, Question
+from polls.models import Poll, TypeAnswer, TextQuestion, Answer, OneQuestion, ManyQuestion
 
 
 @admin.register(Poll)
@@ -9,8 +9,50 @@ class PollAdmin(admin.ModelAdmin):
     list_display_links = 'title',
     list_filter = 'title', 'date_start', 'date_end',
 
-@admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = 'text_q', 'type_q',
-    list_display_links = 'text_q',
-    list_filter = 'text_q', 'type_q',
+
+# @admin.register(Question)
+# class QuestionAdmin(admin.ModelAdmin):
+#     list_display = 'text_q', 'type_question',
+#     list_display_links = 'text_q',
+#     list_filter = 'text_q', 'type_question',
+
+
+@admin.register(TextQuestion)
+class TextQuestionAdmin(admin.ModelAdmin):
+    list_display = 'text',
+    list_display_links = 'text',
+    list_filter = 'text',
+
+
+@admin.register(OneQuestion)
+class OneQuestionAdmin(admin.ModelAdmin):
+    list_display = 'poll',
+    list_display_links = 'poll',
+    list_filter = 'poll',
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+
+    def short_text(self, obj: Answer):
+        if len(obj.text) > 30:
+            return f'{obj.text[:30]}...'
+        return obj.text
+
+    list_display = 'short_text',
+    list_display_links = 'short_text',
+    list_filter = 'text',
+
+
+@admin.register(TypeAnswer)
+class TypeAnswerAdmin(admin.ModelAdmin):
+    list_display = 'title',
+    list_display_links = 'title',
+    list_filter = 'title',
+
+
+@admin.register(ManyQuestion)
+class ManyQuestionAdmin(admin.ModelAdmin):
+    list_display = 'poll', 'text_q',
+    list_display_links = 'poll', 'text_q',
+    list_filter = 'poll', 'text_q',
--- pollsuser/polls/models.py
+++ pollsuser/polls/models.py
@@ -1,22 +1,62 @@
 from django.db import models
 
+class Answer(models.Model):
+    id = models.AutoField(primary_key=True)
+    poll_id = models.IntegerField(blank=False, null=False)
+    question_id = models.IntegerField(blank=False, null=False)
+    text = models.CharField(max_length=500, default=None, blank=True, null=True)
+    checked = models.BooleanField(default=None, blank=True, null=True)
+
 
 class TypeAnswer(models.Model):
     ANSWER_CHOICES = [
-        ('TYPE_1', 'type_1'),
-        ('TYPE_2', 'type_2'),
-        ('TYPE_3', 'type_3'),
+        ('1', 'TextQuestion'),
+        ('2', 'OneQuestion'),
+        ('3', 'ManyQuestion'),
     ]
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, choices=ANSWER_CHOICES, null=True)
 
 
 class Question(models.Model):
-
+    type_question = models.CharField(max_length=100, null=True, default='Question')
     text_q = models.TextField(max_length=1000, verbose_name='Вопрос')
-    type_q = models.ForeignKey(TypeAnswer, on_delete=models.CASCADE)
+    poll = models.ForeignKey('Poll', on_delete=models.CASCADE, default='1')
 
-    def __str__(self):
-        return f'{self.text_q}'
+    class Meta:
+        abstract = True
+        ordering = ['order_id', '-updated_at']
+
+
+class TextQuestion(Question):
+    text = models.TextField(default='', verbose_name='Текстовый ответ')
+
+    def __init__(self, *args, **kwargs):
+        super(TextQuestion, self).__init__(*args, **kwargs)
+        self.type_question = __class__.__name__
+
+    class Meta:
+        indexes = [models.Index(fields=['poll'])]
+
+class OneQuestion(Question):
+    var_answer = models.ManyToManyField('Answer', limit_choices_to=None)
+
+    def __init__(self, *args, **kwargs):
+        super(OneQuestion, self).__init__(*args, **kwargs)
+        self.type_question = __class__.__name__
+
+    class Meta:
+        indexes = [models.Index(fields=['poll'])]
+
+class ManyQuestion(Question):
+    many_answer = models.ManyToManyField('Answer')
+
+    def __init__(self, *args, **kwargs):
+        super(ManyQuestion, self).__init__(*args, **kwargs)
+        self.type_question = __class__.__name__
+
+    class Meta:
+        indexes = [models.Index(fields=['poll'])]
+
 
 
 # Create your models here.
@@ -26,7 +66,7 @@ class Poll(models.Model):
     date_start = models.DateTimeField(auto_now_add=True, verbose_name='Старт опроса')
     date_end = models.DateTimeField(auto_now=True, verbose_name='Конец опроса')
     description = models.TextField(verbose_name='Описание опроса', max_length=1000)
-    select_question = models.ManyToManyField(Question)
+
 
     class Meta:
         ordering = ['-poll_id']
