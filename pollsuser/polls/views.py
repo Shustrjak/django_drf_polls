5233650a62aef34a3f353be7c60fa97b49febe8a
admin <shustrik4ever@rambler.ru>
2021-04-27 01:04:47 +0300

    Добавление фильтра

--- pollsuser/polls/views.py
+++ pollsuser/polls/views.py
@@ -4,18 +4,28 @@ from .models.polls import Poll
 from .models.answers import Answer
 from .models.questions import Question
 from .serializers import PollSerializer, QuestionSerializer, AnswerSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 
 class PollViewSet(ModelViewSet):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ('title',)
 
 
 class AnswerViewSet(ModelViewSet):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ('question_id',)
 
 
 class QuestionViewSet(ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+    filter_backends = [DjangoFilterBackend]
+    field_labels = 'type_question',
+    filterset_fields = ('type_question', 'poll')
+
--- pollsuser/pollsuser/settings.py
+++ pollsuser/pollsuser/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'polls',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -114,3 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'ORDERING_PARAM': ['django_filters.rest_framework.OrderingFilter'],
+}
+
